d95fbae0aca80f595be67afe50bb21d86e0847a6
Samuel Weke <wekesamuel@yahoo.com>
2019-10-30 14:48:33 +0100

    postgress conn

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('../queries');
-
-
-const employeeCtrl = require('../controllers/employee');
-
-router.get('/users', employeeCtrl.getUsers);
 
 module.exports = router;
