407e02081f55a4c653785fa3683c05b71d01edd7
Samuel Weke <wekesamuel@yahoo.com>
2019-10-31 23:45:15 +0100

    connection

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -1,7 +1,7 @@
 const pool = require('../queries');
 
 exports.getUsers = (req,res) => {
-    pool.query('SELECT * FROM users', (error, results) => {
+    pool.query('SELECT * FROM reflections', (error, results) => {
         if (error) {
           throw error
         }
--- backend/models/test.js
+++ backend/models/test.js
@@ -1,36 +1,52 @@
 const pool = require('../queries');
 
 const createTables = () => {
-    const schoolTable = `CREATE TABLE IF NOT EXISTS
-        students(
-          id SERIAL PRIMARY KEY,
-          student_name VARCHAR(128) NOT NULL,
-          student_age INT NOT NULL,
-          student_class VARCHAR(128) NOT NULL,
-          parent_contact VARCHAR(128) NOT NULL,
-          admission_date VARCHAR(128) NOT NULL
-        )`;
-    pool.query(schoolTable)
-      .then((res) => {
-        console.log(res);
-        pool.end();
-      })
-      .catch((err) => {
-        console.log(err);
-        pool.end();
-      });
-  };
+  const queryText =
+    `CREATE TABLE IF NOT EXISTS
+      reflections(
+        id UUID PRIMARY KEY,
+        success VARCHAR(128) NOT NULL,
+        low_point VARCHAR(128) NOT NULL,
+        take_away VARCHAR(128) NOT NULL,
+        created_date TIMESTAMP,
+        modified_date TIMESTAMP
+      )`;
 
-  pool.on('remove', () => {
-    console.log('client removed');
-    process.exit(0);
-  });
-  
-  
-  //export pool and createTables to be accessible  from an where within the application
-  module.exports = {
-    createTables,
-    pool,
-  };
-  
-  require('make-runnable');
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+/**
+ * Drop Tables
+ */
+const dropTables = () => {
+  const queryText = 'DROP TABLE IF EXISTS reflections';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+module.exports = {
+  createTables,
+  dropTables
+};
+
+require('make-runnable');
