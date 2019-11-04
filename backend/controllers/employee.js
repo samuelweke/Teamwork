a13b407b5833872decc07a99570350361bd01cfd
Samuel Weke <wekesamuel@yahoo.com>
2019-11-04 13:16:45 +0100

    used Promises to create user

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -4,14 +4,41 @@ exports.createUser = (req,res) => {
   const { firstname, lastname, email, password,
      gender, job_role, department, address} = req.body;
 
-    pool.query('INSERT INTO employee (firstname, lastname, email, password, gender, job_role, department, address) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [firstname, lastname, email, password,
-      gender, job_role, department, address],
-     (error, results) => {
-      if (error) {
-        throw error
-      }
+  const query = {
+    text: `INSERT INTO 
+      employee (firstname, lastname, email, password, gender, job_role, department, address) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+    values: [firstname, lastname, email, password, gender, job_role, department, address],
+  }
+
+  pool
+    .query(query)
+    .then(() => {
       res.status(201).json({
-        results: "user added successfully"
-      });
+        message: 'Post saved successfully'
       })
+    })
+    .catch((error) => {
+      res.status(400).json({
+      error: error
+      })
+  }); 
+
+    
 };
+
+// exports.createUser = (req,res) => {
+//   const { firstname, lastname, email, password,
+//      gender, job_role, department, address} = req.body;
+
+//     pool.query('INSERT INTO employee (firstname, lastname, email, password, gender, job_role, department, address) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [firstname, lastname, email, password,
+//       gender, job_role, department, address],
+//      (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       res.status(201).json({
+//         results: "user added successfully"
+//       });
+//       })
+// };
