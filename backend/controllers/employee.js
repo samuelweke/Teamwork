52ba4b1c26dc009155400b0242551a1f67d166f2
Samuel Weke <wekesamuel@yahoo.com>
2019-11-04 15:07:24 +0100

    used bcrypt to hash passwords

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -1,28 +1,33 @@
+const bcrypt = require('bcrypt');
 const pool = require('../queries');
 
 exports.createUser = (req, res) => {
-  const {
-    firstname, lastname, email, password,
-    gender, jobRole, department, address,
-  } = req.body;
+  bcrypt.hash(req.body.password, 10).then(
+    (hash) => {
+      const {
+        firstname, lastname, email,
+        gender, jobRole, department, address,
+      } = req.body;
 
-  const query = {
-    text: `INSERT INTO 
+      const query = {
+        text: `INSERT INTO 
       employee (firstname, lastname, email, password, gender, job_role, department, address) 
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-    values: [firstname, lastname, email, password, gender, jobRole, department, address],
-  };
+        values: [firstname, lastname, email, hash, gender, jobRole, department, address],
+      };
 
-  pool
-    .query(query)
-    .then(() => {
-      res.status(201).json({
-        message: 'User created successfully',
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error,
-      });
-    });
+      pool
+        .query(query)
+        .then(() => {
+          res.status(201).json({
+            message: 'User created successfully',
+          });
+        })
+        .catch((error) => {
+          res.status(400).json({
+            error,
+          });
+        });
+    },
+  );
 };
