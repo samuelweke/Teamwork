40f0c43a6500d4c10687367415a30fa18db63a0f
Samuel Weke <wekesamuel@yahoo.com>
2019-11-13 12:59:06 +0100

    eslint fix

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,8 +1,8 @@
 const pool = require('../queries');
 
 exports.getFeed = (req, res) => {
-    const query = {
-        text: `
+  const query = {
+    text: `
         SELECT * FROM (
             SELECT article_id as id, title, article.article as post, user_id, created_on
             FROM article
@@ -10,20 +10,20 @@ exports.getFeed = (req, res) => {
             SELECT gif_id as id, title, url as post, user_id, created_on
             FROM gif
         ) AS feed ORDER BY created_on DESC`,
-    }
-    pool
-      .query(query)
-      .then((feedTable) => {
-        res.status(201).json(
-            feedTable.rows.map((feed) => ({
-                id: feed.id,
-                createdOn: feed.created_on,
-                title: feed.title,
-                article: feed.post,
-                article: feed.post,
-                authorId: feed.user_id,
-            })),
-        )
-      })
-      .catch(error => res.status(401).json({error}));
+  };
+  pool
+    .query(query)
+    .then((feedTable) => {
+      res.status(201).json(
+        feedTable.rows.map((feed) => ({
+          id: feed.id,
+          createdOn: feed.created_on,
+          title: feed.title,
+          article: feed.post,
+          article: feed.post,
+          authorId: feed.user_id,
+        })),
+      );
+    })
+    .catch((error) => res.status(401).json({ error }));
 };
