bb1405b8abc22b0daf57098baa1e3687af95cce5
Samuel Weke <wekesamuel@yahoo.com>
2019-11-20 12:24:44 +0100

    qa

--- routes/article.js
+++ routes/article.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const articleCtrl = require('../controllers/article');
 const auth = require('../middleware/auth');
 
-router.post('/', auth, articleCtrl.createArticle);
-router.patch('/:id', auth, articleCtrl.editArticle);
+router.post('/',  articleCtrl.createArticle);
+router.patch('/:id',  articleCtrl.editArticle);
 router.delete('/:id', auth, articleCtrl.deleteArticle);
 router.post('/:id/comment', auth, articleCtrl.createComment);
-router.get('/:id', auth, articleCtrl.getOneArticle);
+router.get('/:id',  articleCtrl.getOneArticle);
 
 
 module.exports = router;
